73a1396c1d182b026a95ffa666f022598d0f7428
Red diamond <229543222+dileshwars927-oss@users.noreply.github.com>
2026-05-16 05:50:46 +0530

    Add core/__init__.py

---
+++ DileshOS_Bharat/core/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+core/__init__.py - Core module initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "DileshOS Team"
+__description__ = "DileshOS Bharat Edition - Core Module"
